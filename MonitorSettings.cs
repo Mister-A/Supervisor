cf009d05a4095394507a1ed965f7f3bd2f5e31c9
Andrew Aldridge <andrew@aldridgesolutions.co.uk>
2022-02-07 12:21:28 +0000

    Converted to using config file for monitor list

---
+++ MonitorSettings.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Configuration;
+using System.Diagnostics;
+using System.Windows.Forms;
+using static System.Environment;
+
+namespace Supervisor
+{
+    /// <summary>
+    /// Loads the application settings file from ProgramData path
+    /// </summary>
+    internal class MonitorSettings
+    {
+        private string localSettingsPath = GetFolderPath(SpecialFolder.CommonApplicationData) + "\\Supervisor\\";
+
+        internal MonitorSettings()
+        {
+            ExeConfigurationFileMap configMap = new ExeConfigurationFileMap();
+            configMap.ExeConfigFilename = localSettingsPath + @"settings.config";
+            try
+            {
+                monitorConfig = ConfigurationManager.OpenMappedExeConfiguration(configMap, ConfigurationUserLevel.None);
+                monitorGroup = monitorConfig.GetSection("MonitorGroup") as MonitorGroup;
+                monitors = monitorGroup.Monitors;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Supervisor: Exception opening settings.config: " + ex.Message);
+                int errorCode = -1;
+                if (ex.InnerException != null)
+                {
+                    errorCode = ex.InnerException.HResult & 0xFFFF;
+                }
+                else
+                {
+                    errorCode = ex.HResult & 0xFFFF;
+                }
+                string message = "Your settings.config file appears to be corrupt, you may need to delete it or reinstall this program\r\n (" + ex.Message + ")";
+                string caption = "Corrupt settings detected";
+                MessageBoxButtons buttons = MessageBoxButtons.OK;
+                DialogResult result;
+
+                // Displays the MessageBox.
+                result = System.Windows.Forms.MessageBox.Show(message, caption, buttons, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+                //Close app
+                Environment.Exit(1);
+            }
+        }
+
+        public Configuration monitorConfig { get; set; }
+        public MonitorGroup monitorGroup { get; set; }
+        public MonitorGroup.MonitorCollection monitors { get; set; }
+
+        /// <summary>
+        /// Triggers current settings to be written to file
+        /// </summary>
+        public bool SaveMonitorSettings()
+        {
+            monitorConfig.Save(ConfigurationSaveMode.Modified);
+            return true;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,142 +1,72 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
-namespace Supervisor {
-    public class Program {
-        static void Main(string[] args) {
-            var programArgs = ParseArgs(args);
-            if (programArgs.Count == 0) {
-                Console.WriteLine("Please specify applications to start and watch. For example:");
-                Console.WriteLine("Start and monitor one app:");
-                Console.WriteLine("supervisor myapp.exe");
-                Console.WriteLine("Start and monitor two apps:"); 
-                Console.WriteLine("supervisor app1.exe app2.exe");
-                Console.WriteLine("Start and monitor two apps with arguments:");
-                Console.WriteLine("supervisor \"app1.exe \"\"argument 1\"\"\" \"\"\"Folder Name\\app1.exe\"\" \"\"argument 1\"\"\"");
-                return;
-            }
+namespace Supervisor
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            MonitorSettings settings = new MonitorSettings();
+            var threads = new List<MonitorThread>(settings.monitorGroup.Monitors.Count);
 
-            var threads = new List<MonitorThread>(programArgs.Count);
-
-            for (int i = 0; i < programArgs.Count; i++) {
-                var t = new MonitorThread(programArgs[i]);
+            foreach (MonitorGroup.ApplicationElement appMonitor in settings.monitorGroup.Monitors)
+            {
+                var t = new MonitorThread(appMonitor);
                 threads.Add(t);
             }
-
-            Console.WriteLine("Press enter to stop all processes.");
-            Console.ReadLine();
-            for (int i = 0; i < threads.Count; i++) {
-                threads[i].Stop();
-            }
-            for (int i = 0; i < threads.Count; i++) {
-                threads[i].Join();
+            Thread.Sleep(5000);
+            for (int i = 0; i < threads.Count; i++)
+            {
+                threads[i].IsCancelled = true;
             }
         }
-
-        public static List<ProgramArgs> ParseArgs(string[] args) {
-            var res = new List<ProgramArgs>(args.Length);
-            for (int i = 0; i < args.Length; i++) {
-                var arg = args[i];
-                if (arg[0] == '\"') {
-                    var quoteIndex = arg.IndexOf('\"', 1);
-                    res.Add(new ProgramArgs {
-                        ExeName = arg.Substring(1, quoteIndex - 1),
-                        Arguments = arg.Substring(quoteIndex + 1).Trim()
-                    });
-                } else {
-                    var spaceIndex = arg.IndexOf(' ');
-                    if (spaceIndex < 0) {
-                        res.Add(new ProgramArgs {
-                            ExeName = arg,
-                            Arguments = ""
-                        });
-                    } else {
-                        res.Add(new ProgramArgs {
-                            ExeName = arg.Substring(0, spaceIndex),
-                            Arguments = arg.Substring(spaceIndex + 1).Trim()
-                        });
-                    }
-                }
-            }
-            return res;
-        }
-
     }
 
-    public class MonitorThread {
-        public readonly ProgramArgs Args;
-        
-        public Process Process {
+    public class MonitorThread
+    {
+        public readonly MonitorGroup.ApplicationElement Args;
+
+        public Process Process
+        {
             get;
             private set;
         }
 
-        private bool IsCancelled;
+        public bool IsCancelled;
 
         private readonly Thread Thread;
 
-        public MonitorThread(ProgramArgs args) {
-            Args = args;
+        public MonitorThread(MonitorGroup.ApplicationElement monitor)
+        {
+            Args = monitor;
             Thread = new Thread(DoMonitor);
             Thread.Start();
         }
 
-        private void DoMonitor() {
-            Process = Process.Start(Args.ExeName, Args.Arguments);
-            while (true) {
-                if (Process.WaitForExit(1000) && !IsCancelled) {
-                    Process = Process.Start(Args.ExeName, Args.Arguments);
+        private void DoMonitor()
+        {
+            Process = Process.Start(Args.Path, Args.Arguments);
+            while (true)
+            {
+                if (Process.WaitForExit(1000) && !IsCancelled)
+                {
+                    Process = Process.Start(Args.Path, Args.Arguments);
                 }
-                if (IsCancelled) {
-                    if (!Process.HasExited)
-                        Process.Kill();
-                    return;
+                if (IsCancelled)
+                {
+                    Process.Kill();
+                    Process.WaitForExit();
+                    break;
                 }
+                Thread.Sleep(2000);
             }
         }
 
-        public void Stop() {
+        public void Stop()
+        {
             IsCancelled = true;
-        }
-
-        public void Join() {
-            if (Thread.IsAlive)
-                Thread.Join();
-        }
-
-        public bool Join(TimeSpan timeout) {
-            if (Thread.IsAlive)
-                return Thread.Join(timeout);
-            else
-                return true;
-        }
-    }
-
-    public class ProgramArgs {
-        public string ExeName;
-        public string Arguments;
-
-        public override bool Equals(object obj) {
-            if (obj is ProgramArgs) {
-                var b = obj as ProgramArgs;
-                return ExeName == b.ExeName && Arguments == b.Arguments;
-            } else
-                return false;
-        }
-
-        public override string ToString() {
-            return "\"" + ExeName + "\" " + Arguments;
-        }
-
-        public override int GetHashCode() {
-            var result = ExeName.GetHashCode();
-            result = 31 * result + Arguments.GetHashCode();
-            return result;
         }
     }
 }
